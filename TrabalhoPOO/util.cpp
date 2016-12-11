eebef635ffb05296e4c12d9cc204083cc9332136
FilipeA <filipera1995@hotmail.com>
2016-12-11 23:06:16 +0000

    funçao-> filtrar comando <- adicionada

--- TrabalhoPOO/util.cpp
+++ TrabalhoPOO/util.cpp
@@ -29,26 +29,18 @@ string cin_str(){
 
 int filtra_comando(string cmd){
     int escolha = 0;
-	if (cmd == "dim")
-		escolha = 1;
-	else if (!cmd.compare("moedas"))
-		escolha = 2;
-	else if (!cmd.compare("oponentes"))
-		escolha = 3;
-	else if (!cmd.compare("castelo"))
-		escolha = 4;
-	else if (!cmd.compare("mkperfil"))
-		escolha = 5;
-	else if (!cmd.compare("addperfil"))
-		escolha = 6;
-	else if (!cmd.compare("subperfil"))
-		escolha = 7;
-	else if (!cmd.compare("rmperfil"))
-		escolha = 8;
-	else if (!cmd.compare("load"))
-		escolha = 9;
-	else if (!cmd.compare("inicio"))
-		escolha = 10;
-	return escolha;
+    
+	if      (cmd == "dim")          escolha = 1;
+	else if (cmd == "moedas")       escolha = 2;
+	else if (cmd == "oponentes")    escolha = 3;
+	else if (cmd == "castelo")      escolha = 4;
+	else if (cmd == "mkperfil")     escolha = 5;
+	else if (cmd == "addperfil")    escolha = 6;
+	else if (cmd == "subperfil")    escolha = 7;
+	else if (cmd == "rmperfil")     escolha = 8;
+	else if (cmd == "load")         escolha = 9;
+	else if (cmd == "inicio")       escolha = 10;
+    
+    return escolha;
 }
     
